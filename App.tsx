ca140482b2612ed4dafcf9acec70cd8c3cf20496
unknown <masects@gmail.com>
2023-12-30 15:27:47 -0500

    Day1N

--- App.tsx
+++ App.tsx
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('Hello');
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to on your app!</Text>
+      <Text>Open up App.tsx to on your app!</Text>
+      <Text>Open up App.tsx to on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
